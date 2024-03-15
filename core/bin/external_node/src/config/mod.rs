41340d0b34b8fd4501132c27b9d650cc49d9ada5
tomg10 <lemures64@gmail.com>
2024-03-15 02:40:06 +0100

    round of fixes
    Signed-off-by: tomg10 <lemures64@gmail.com>

--- core/bin/external_node/src/config/mod.rs
+++ core/bin/external_node/src/config/mod.rs
@@ -243,11 +243,14 @@ pub struct OptionalENConfig {
     // its purpose; the consistency checker assumes that the main node may provide false information.
     pub contracts_diamond_proxy_addr: Option<Address>,
 
+    #[serde(default = "OptionalENConfig::default_snapshots_recovery_enabled")]
+    pub snapshots_recovery_enabled: bool,
+
     #[serde(default = "OptionalENConfig::default_pruning_chunk_size")]
     pub pruning_chunk_size: u32,
 
     /// If set, l1 batches will be pruned after they are that long
-    pub l1_batch_age_to_prune_hours: Option<u64>,
+    pub pruning_data_retention_hours: Option<u64>,
 }
 
 impl OptionalENConfig {
@@ -352,6 +355,10 @@ impl OptionalENConfig {
 
     const fn default_miniblock_seal_queue_capacity() -> usize {
         10
+    }
+
+    const fn default_snapshots_recovery_enabled() -> bool {
+        false
     }
 
     const fn default_pruning_chunk_size() -> u32 {
